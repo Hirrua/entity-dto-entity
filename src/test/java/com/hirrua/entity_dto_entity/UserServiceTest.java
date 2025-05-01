86e849082dae9f14fd80c28cc15bc492c8d799d5
Hirruá Souza da Silva <96480452+Hirrua@users.noreply.github.com>
2025-05-01 14:13:03 -0300

    Test/create first test (#4)
    * feat: get user by document
    
    * test: create my first test with JUnit and Mockito

---
+++ src/test/java/com/hirrua/entity_dto_entity/UserServiceTest.java
@@ -0,0 +1,42 @@
+package com.hirrua.entity_dto_entity;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void deveRetornarUmUsuarioComCpf() {
+        String cpf = "12345678900";
+        UserEntity userEntityMock = new UserEntity();
+        userEntityMock.setCpf(cpf);
+        userEntityMock.setNome("Nome Teste");
+        userEntityMock.setSobrenome("Fulano Teste");
+        userEntityMock.setEmail("fulanoteste@gmail.com");
+        userEntityMock.setCelular("555555555");
+        userEntityMock.setUserStatus(UserStatus.ACTIVE);
+
+        when(userRepository.searchByDocument(cpf)).thenReturn(userEntityMock);
+
+        UserDto resultado = userService.getByDocument(cpf);
+
+        assertNotNull(resultado);
+        assertEquals(cpf, resultado.cpf());
+        assertEquals("555555555", resultado.celular());
+        assertEquals("fulanoteste@gmail.com", resultado.email());
+    }
+}
